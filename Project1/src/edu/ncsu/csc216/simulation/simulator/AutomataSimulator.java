4628053a2f6d1ec38f87114428f572ab99eee562
NBoar <NBoar@DESKTOP-U79JFG9>
2016-09-24 17:55:22 -0400

    Inserted placeholder values in AutomataSimulator

--- Project1/src/edu/ncsu/csc216/simulation/simulator/AutomataSimulator.java
+++ Project1/src/edu/ncsu/csc216/simulation/simulator/AutomataSimulator.java
@@ -5,14 +5,16 @@ import edu.ncsu.csc216.simulation.environment.utils.PaintedLocation;
 
 public class AutomataSimulator implements SimulatorInterface {
 
-	private static final int SIZE;
-	private static final int THRESHOLD;
-	private static final String SIZE_ERROR_MESSAGE;
-	private static final String THRESHOLD_ERROR_MESSAGE;
+	
+	//PLACEHOLDER VALUES FOR THE FINALS.  WHAT ARE THE REAL ONES?
+	private static final int SIZE = 0;
+	private static final int THRESHOLD = 0;
+	private static final String SIZE_ERROR_MESSAGE = "";
+	private static final String THRESHOLD_ERROR_MESSAGE = "";
 	private String[] names;
 	private int numberOfNames;
 	private char[] symbol;
-	private static final char EMPTY;
+	private static final char EMPTY = ' ';
 	private EcoGrid simpleSystem;
 
 
