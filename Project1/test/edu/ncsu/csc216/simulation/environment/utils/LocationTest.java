1968a5e22913962b170a36e4700914dfeacfea6e
NBoar <NBoar@DESKTOP-U79JFG9>
2016-10-04 21:36:02 -0400

    Implemented PaintedLocationTest and updated LocationTest

--- Project1/test/edu/ncsu/csc216/simulation/environment/utils/LocationTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/environment/utils/LocationTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author NBoar
+ * Test class for the Location class.
+ */
 public class LocationTest {
 
 	
+	/**
+	 * Tests the Location constructor.
+	 * Also tests the getter methods for Location.
+	 */
 	@Test
-	public void LocationTest() {
+	public void locationTest() {
 		Location l = new Location(12,15);
 		
 		assertEquals(12, l.getRow());
--- Project1/test/edu/ncsu/csc216/simulation/environment/utils/PaintedLocationTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/environment/utils/PaintedLocationTest.java
@@ -1,5 +1,30 @@
 package edu.ncsu.csc216.simulation.environment.utils;
 
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
+/**
+ * @author NBoar
+ * Test class for the PaintedLocation class.
+ */
 public class PaintedLocationTest {
 
+	/**
+	 * Tests the PaintedLocation constructor.
+	 * Also tests the getter methods for PaintedLocation.
+	 */
+	@Test
+	public void paintedLocationTest() {
+		PaintedLocation l = new PaintedLocation(2, 3, Color.MAGENTA, '@');
+		
+		assertEquals(2, l.getRow());
+		assertEquals(3, l.getCol());
+		assertEquals(Color.MAGENTA, l.getColor());
+		assertEquals('@', l.getSymbol());
+	}
+	
+	
 }
