51c9007d1ffbe26c9d9bb04f13528358b839cf61
NBoar <NBoar@DESKTOP-U79JFG9>
2016-10-04 21:27:18 -0400

    Added junit 4 to classpath and LocationTest

--- Project1/test/edu/ncsu/csc216/simulation/environment/utils/LocationTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/environment/utils/LocationTest.java
@@ -1,5 +1,18 @@
 package edu.ncsu.csc216.simulation.environment.utils;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class LocationTest {
 
+	
+	@Test
+	public void LocationTest() {
+		Location l = new Location(12,15);
+		
+		assertEquals(12, l.getRow());
+		assertEquals(15, l.getCol());
+	}
+	
 }
