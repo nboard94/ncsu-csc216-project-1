5436863a50811f4ad3a9fa01754b2d0a4228a1db
NBoar <NBoar@192.168.0.17>
2016-10-09 15:18:35 -0400

    More testing on AutomataSim and PurePrey

--- Project1/test/edu/ncsu/csc216/simulation/actor/PurePreyTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/actor/PurePreyTest.java
@@ -1,5 +1,19 @@
 package edu.ncsu.csc216.simulation.actor;
 
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
 public class PurePreyTest {
 
+	
+	@Test
+	public void purePreyTest() {
+		PurePrey p = new PurePrey('f');
+		
+		assertEquals(Color.GREEN, p.getColor());
+		
+	}
 }
--- Project1/test/edu/ncsu/csc216/simulation/simulator/AutomataSimulatorTest.java
+++ Project1/test/edu/ncsu/csc216/simulation/simulator/AutomataSimulatorTest.java
@@ -1,9 +1,34 @@
 package edu.ncsu.csc216.simulation.simulator;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**Tests the AutomataSimulator class
  * @author NBoar
  *
  */
 public class AutomataSimulatorTest {
 
+	@Test
+	public void automataSimulatorTest() {
+
+		try {
+			AutomataSimulator as = new AutomataSimulator("/Project1/test-files/testFile");
+			assertEquals(4, as.getNames().length);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void automataSimulatorConfigTest() {
+
+		try {
+			AutomataSimulator as = new AutomataSimulator("/Project1/test-files/testFile", "/Project1/test-files/configTest");
+			assertEquals(4, as.getNames().length);
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
 }
